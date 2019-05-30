d3c0d884795664edeed6fbe9d762e5405df439f7
IkramMissaoui <missaoui.ikram726@gmail.com>
2019-05-30 15:05:07 +0200

    Updates

---
+++ precache-manifest.d586fc17742dcedbf157452a3e5df2c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e4b39282878b4d5da4ae9fb80bff0b9",
+    "url": "/MovieList/index.html"
+  },
+  {
+    "revision": "04585ff75643f871af8e",
+    "url": "/MovieList/static/css/main.2f54e193.chunk.css"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/MovieList/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "04585ff75643f871af8e",
+    "url": "/MovieList/static/js/main.85f6d2ff.chunk.js"
+  },
+  {
+    "revision": "3d6ad88b6f7905c89428",
+    "url": "/MovieList/static/js/runtime~main.3318021b.js"
+  }
+]);
